2aa73836e05d84d7ab9bf3ec50e787e03d94be41
Collabnix <34368930+collabnix@users.noreply.github.com>
2024-08-16 12:01:19 +0530

    Create test_customers.py

---
+++ tests/test_customers.py
@@ -0,0 +1,51 @@
+import os
+import pytest
+from testcontainers.postgres import PostgresContainer
+
+from customers import customers
+
+postgres = PostgresContainer("postgres:16-alpine")
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(request):
+    """
+    Setup the test environment
+
+    This fixture will only run once for all the tests in the module.
+
+    :param request:
+    :return:
+    """
+    postgres.start()
+
+    def remove_container():
+        postgres.stop()
+
+    request.addfinalizer(remove_container)
+    os.environ["DB_CONN"] = postgres.get_connection_url()
+    os.environ["DB_HOST"] = postgres.get_container_host_ip()
+    os.environ["DB_PORT"] = postgres.get_exposed_port(5432)
+    os.environ["DB_USERNAME"] = postgres.username
+    os.environ["DB_PASSWORD"] = postgres.password
+    os.environ["DB_NAME"] = postgres.dbname
+    customers.create_table()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_data():
+    customers.delete_all_customers()
+
+
+def test_get_all_customers():
+    customers.create_customer("Siva", "siva@gmail.com")
+    customers.create_customer("James", "james@gmail.com")
+    customers_list = customers.get_all_customers()
+    assert len(customers_list) == 2
+
+
+def test_get_customer_by_email():
+    customers.create_customer("John", "john@gmail.com")
+    customer = customers.get_customer_by_email("john@gmail.com")
+    assert customer.name == "John"
+    assert customer.email == "john@gmail.com"
